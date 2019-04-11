e4123bd41b1ccf512e1cc4d81f6066364836d5ec
JoseAngelPerez <48016255+JoseAngelPerez@users.noreply.github.com>
2019-04-11 18:16:25 -0500

    Add files via upload

---
+++ Assets/Scripts/Game/Pool/Pooler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pooler : MonoBehaviour
+{
+    [SerializeField] GameObject[] spawnPoints= new GameObject[3];
+
+    [SerializeField]
+    GameObject[] enemies;
+
+    private int[] tomados;
+    public bool thereAreEnemies;
+    public int restantes;
+
+
+
+
+     void Start()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("enemy");
+        thereAreEnemies = false;
+        
+    }
+
+    
+    void Update()
+    {
+        if(!thereAreEnemies)
+        {
+            int enemiesNumber;
+            enemiesNumber = Random.Range(1, enemies.Length);
+            restantes = enemiesNumber;
+
+            for(int i=0; i<enemiesNumber-1;i++)
+            {
+                int SpawnPointNumber;
+                SpawnPointNumber = Random.Range(0, 3);
+
+                enemies[i].transform.position = spawnPoints[SpawnPointNumber].transform.position;
+            }
+
+            thereAreEnemies = true;
+        }
+
+        if(restantes==0)
+        {
+            thereAreEnemies = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/killenemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killenemy : MonoBehaviour
+{
+    [SerializeField] GameObject pool;
+    [SerializeField] GameObject pooler;
+
+    private void Start()
+    {
+        pool= GameObject.FindGameObjectWithTag("pool");
+        pooler = GameObject.FindGameObjectWithTag("pooler");
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag ==("player"))
+        {
+            transform.position = pool.transform.position;
+            pooler.GetComponent<Pooler>().restantes= pooler.GetComponent<Pooler>().restantes-1;
+        }
+    }
+}
